5b004486d79405c889854198373128ce92a248b9
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-21 23:37:37 -0300

    Ultimo commit

--- user/carrinho.php
+++ user/carrinho.php
@@ -4,10 +4,11 @@
     include '../Config/conexao.php';
     session_start();
     $iduser=$_SESSION['id'];
-    $sql="SELECT pedido.idProduto AS idp, produto.nome as nome, produto.preco as preco, produto.foto as foto FROM produto INNER JOIN pedido ON pedido.idProduto=produto.id INNER JOIN cliente ON pedido.idCliente=cliente.id WHERE cliente.id=$iduser";
+    $sql="SELECT pedido.quantidade as quant, pedido.idProduto AS idp, produto.nome as nome, produto.preco as preco, produto.foto as foto FROM produto INNER JOIN pedido ON pedido.idProduto=produto.id INNER JOIN cliente ON pedido.idCliente=cliente.id WHERE cliente.id=$iduser";
     $res=$cn->query($sql);
 ?>
 <!DOCTYPE html>
+<h1 style="text-align: center;">CARRINHO</h1>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -26,14 +27,19 @@
         <tr>
             <th>NOME</th>
             <th>PRECO</th>
+            <th>QUANT</th>
             <th>FOTO</th>
+            <th>VALOR TOTAL</th>
         </tr>
         <?php while($dado = $res->fetch_array()){ ?>
             <tr>
                 <th><?php echo $dado['nome']; ?></th>
-                <th><?php echo $dado['preco']; ?></th>
+                <th>R$<?php echo $dado['preco']; ?>.00</th>
+                <th><?php echo $dado['quant'] ?></th>
                 <th><img src="<?php echo $dado['foto']; ?>" ></th>
-                <th><form action="./processos/remCarrinho.php" method="post"><button name="idp" value="<?php echo $dado['idp'] ?>">DEL</button></form></th>
+                <th>R$<?php echo $dado['preco']*$dado['quant'];?>.00</th>
+                <th><form action="./processos/remCarrinho.php" method="post"><button style="width: 50px;
+        height: 50px;" name="idp" value="<?php echo $dado['idp'] ?>">DEL</button></form></th>
             </tr>
         <?php } ?>
     </table>
