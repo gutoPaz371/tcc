b4d3201df697dccf9654613e9e1a38afb2773c46
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-24 16:52:35 -0300

    rm

--- index.php
+++ index.php
@@ -25,7 +25,6 @@
     <nav id="menu-1">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="teste.php">fds</a></li>
                 <li><a href="produtos.php">Produtos</a></li>
                 <li><a href="#######">Empresa</a></li>
                 <li><a href="./user/carrinho.php">Carrinho</a></li>
