42bc254c43a2babc13133e775d9414a47f7cc95c
Daniel Hughes <danielhughes589@outlook.com>
2021-05-24 16:49:51 -0300

    Atualização na Pag Produtos,PHP - Daniel Hughes Araujo

--- produtos.php
+++ produtos.php
@@ -12,27 +12,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="./css/Produtos.css">
 </head>
-<style>
-    img{
-        width: 200px;
-        height: 150px;
-        text-align: center;
-    }
-    #raiz{
-        background-color: red;
-        margin: 20px;
-        float: left;
-        width: 200px;
-        height: 300px;
-    }
-    #ft{
-        width: 150px;
-        height: 150px;
-        text-align: center;
-    }
-</style>
 <body>
+<a href="index.php"><button style="text-align: left;">Voltar</button></a>
+
+
     <h1 style="text-align: center;">Estoque</h1>
         <?php while($dado = $res->fetch_array()){ ?>    
             <div id="raiz">
@@ -40,11 +26,10 @@
                 <div id="ft"><img src="<?php echo $dado['foto'] ?>"></div><br>
                 <div id="inf"><p>VALOR</p><P>R$ <?php echo $dado['preco'] ?></P></div>
                 <div><form action="./user/processos/addCarrinho.php" method="POST">
-                <button name="idp" value="<?php echo $dado['id'] ?>" type="submit" style="width: 200px;height: 50px;">+</button>
+                <button name="idp" value="<?php echo $dado['id'] ?>" type="submit" style="width: 200px;height: 50px;">Comprar</button>
                 </form></div>
             </div> 
         <?php } ?>
-    <a href="index.php"><button style="text-align: left;">Voltar</button></a>
 </body>
 </html>
 <!--CODIGO PRODUZIDO POR AUGUSTO OLIVEIRA PAZ 201902535855-->
