c926d83d2434ad408006e3edac96bb04ef048c05
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-25 10:34:50 -0300

    Atualizaçao cadastro

--- user/cadastro.php
+++ user/cadastro.php
@@ -25,29 +25,55 @@
 	</div>
     <div id="contL">
         <h1>Cadastre-se</h1>
-        <form action="">
+        <form action="./processos/processaCadastro.php" method="POST">
             
             <label for="name">Nome:</label>
-            <input type="name" name="name" id="name" placeholder="Digite seu nome.">
+            <input type="name" name="user" id="name" placeholder="Digite seu nome.">
+            
+            <label for="name">CPF:</label>
+            <input type="name" name="CPF" id="CPF" onkeypress="cpf(this)" maxlength="14" placeholder="000.000.000-00">
 
             <label for="celular">Contato:</label>
-            <input type="celular" name="celular" id="celular" placeholder="Digite seu número.">
+            <input type="celular" name="celular" id="celular" onkeypress="cll(this)" maxlength="14" placeholder="(99) 9999-9999">
 
             <label for="email">Email:</label>
             <input type="email" name="email" id="email" placeholder="Digite seu email.">
 
             <label for="confEmail">Cofirmar Email:</label>
-            <input type="confEmail" name="confEmail" id="confEmail" placeholder="Confirme seu email.">
+            <input type="confEmail" name="email1" id="confEmail" placeholder="Confirme seu email.">
 
             <label for="password">Senha:</label>
-            <input type="password" name="password" id="password" placeholder="Digite Sua Senha.">
+            <input type="password" name="pass" id="password" placeholder="Digite Sua Senha.">
 
             <label for="confSenha">Confirme sua Senha:</label>
-            <input type="confSenha" name="confSenha" id="confSenha" placeholder="Confirme sua senha.">
+            <input type="password" name="pass1" id="confSenha" placeholder="Confirme sua senha.">
 
             <input type="submit" value="Enviar">
 
         </form>
     </div>
 </body>
+<script>
+    function cll(){
+        let nume=document.getElementById('celular').value;
+        if(nume.length==0){
+            document.getElementById('celular').value='('+nume;
+        }else if(nume.length==3){
+            document.getElementById('celular').value=nume+') ';
+        }else if(nume.length==9){
+            document.getElementById('celular').value=nume+'-';
+        }
+    }
+    function cpf(){
+        let nume=document.getElementById('CPF').value;
+        console.log(nume.length);
+        if(nume.length==3){
+            document.getElementById('CPF').value=nume+'.';
+        }else if(nume.length==7){
+            document.getElementById('CPF').value=nume+'.';
+        }else if(nume.length==11){
+            document.getElementById('CPF').value=nume+'-';
+        }
+    }
+</script>
 </html>
--- user/processos/processaCadastro.php
+++ user/processos/processaCadastro.php
@@ -5,8 +5,11 @@
     session_start();
     $user=$_POST['user'];
     $emai=$_POST['email'];
+    $emai1=$_POST['email1'];
     $pass=$_POST['pass'];
     $pass1=$_POST['pass1'];
+    $cll=$_POST['celular'];
+    $cpf=$_POST['CPF'];
     if($pass!=$pass1){
         $_SESSION['erro']='Senhas Nao coicidem!!';
         $_SESSION['cor']='red';
@@ -17,7 +20,7 @@
         $_SESSION['cor']='red';
         header("location:../cadastro.php");
     }else{
-        $sql="INSERT INTO cliente(nome,email,senha) VALUES ('$user','$emai','$pass')";
+        $sql="INSERT INTO cliente(nome,email,senha,celular,cpf) VALUES ('$user','$emai','$pass','$cll','$cpf')";
         $cn->query($sql);
         $_SESSION['erro']='Cadastrado';
         $_SESSION['cor']='green';
