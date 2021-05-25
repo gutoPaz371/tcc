ca1484ad5a8e796d3ec3574bc4b3e3849fbb331a
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-25 11:23:44 -0300

    Implementacao ao banco

--- produtos.php
+++ produtos.php
@@ -5,35 +5,53 @@
     session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Mania de Festa</title>
+      <link rel="stylesheet" href="css/prateleira.css">
 
-    <link rel="stylesheet" href="./css/Produtos.css">
 </head>
 <body>
     <!-- inicio menu -->
     <nav id="menu-1">
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="user/carrinho.php">Carrinho</a></li>
+                <li><a href="####">Produtos</a></li>
+                <li><a href="####">Empresa</a></li>
             </ul>
     </nav>
     <!--  final menu -->
 
-        <?php while($dado = $res->fetch_array()){ ?>    
-            <div id="raiz">
-                <p style="text-align: center;"><?php echo $dado['nome'] ?></p>
-                <div id="ft"><img src="./img/<?php echo $dado['id'] ?>.png"></div><br>
-                <div id="inf"><p>VALOR</p><P>R$ <?php echo $dado['preco'] ?></P></div>
-                <div><form action="./user/processos/addCarrinho.php" method="POST">
-                <button name="idp" value="<?php echo $dado['id'] ?>" type="submit" style="width: 200px;height: 50px;">Comprar</button>
-                </form></div>
-            </div> 
-        <?php } ?>
-</body>
-</html>
+        <link rel="stylesheet" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Girassol&display=swap" rel="stylesheet">
+    <main>
+
+                     <!-- inicio prateleira -->
+               <div class="prateleira">
+                   <ul class="prateleira ul">
+                        <?php while($dado = $res->fetch_array()){ ?>
+                            <li><h1><?php echo $dado['nome'] ?></h1><img src="./img/<?php echo $dado['id']?>.png"><h2>R$ <?php echo $dado['preco'] ?>.00</h2><form action="./user/processos/addCarrinho.php" method="POST">
+                <button name="idp" value="<?php echo $dado['id'] ?>" type="submit" class="botao-ver-prateleira">Adicionar ao Carrinho</button>
+                </form></li>
+                        <?php } ?>
+                   </ul>
+               </div>
+    </main>
+                     <!--final prateleira-->
+
+        
+
+
+                                <!--  inicio rodapé -->
+
+             <footer class="rodape"> <h1>www.maniadefesta.com.br</h1></footer>
+                 <link rel="preconnect" href="https://fonts.gstatic.com">
+                 <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+                
+                                                <!--  final rodapé -->
+ </body>
+ </html> 
 <!--CODIGO PRODUZIDO POR AUGUSTO OLIVEIRA PAZ 201902535855-->
