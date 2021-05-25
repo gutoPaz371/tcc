c81928ed800dbf4ffc7c5f7af2c2cfcf34afe174
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-25 12:52:02 -0300

    guto

--- painel/admin/crudAdm/pedidos.php
+++ painel/admin/crudAdm/pedidos.php
@@ -1,11 +1,12 @@
 <!--CODIGO PRODUZIDO POR AUGUSTO OLIVEIRA PAZ 201902535855-->
 <?php
     include '../../../Config/conexao.php';
-    $sql="SELECT cliente.nome AS user, produto.nome, produto.preco, pedido.quantidade, pedido.sta, produto.preco*pedido.quantidade AS total
+    $sql="SELECT cliente.nome AS user, produto.nome, produto.preco, pedido.quantidade, pedido.sta as sta, produto.preco*pedido.quantidade AS total
     FROM cliente
     INNER JOIN pedido ON cliente.id=pedido.idCliente
     INNER JOIN produto ON pedido.idProduto=produto.id";
     $res=$cn->query($sql);
+    $x=mysqli_fetch_assoc($res);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,12 +18,19 @@
     <title>Document</title>
 </head>
 <body>
-    <?php while($dado = $res->fetch_array()){ ?>    
+    <?php while($dado = $res->fetch_array()){ 
+        if($x['sta']==1){
+            $st='COMFIRMADO';
+        }else if($x['sta']==0){
+            $st='PENDENTE';
+        }
+            ?>    
             <div id="raiz">
-                <p style="text-align: center;"><?php echo $dado['nome'] ?></p>
-                <!--<div id="ft"><img src="./img/<?php #echo $dado['id'] ?>.png"></div><br>-->
-                <div id="inf"><p>CLIENTE</p><P><?php echo $dado['user'] ?></P></div>
-                <div id="inf"><p>VALOR</p><P>R$ <?php echo $dado['preco'] ?></P></div>
+                <!--<div id="ft"><img src="../../../img/<?php #echo $dado['id'] ?>.png"></div><br>-->
+                <div id="inf"><p><?php echo $dado['user'] ?></P></div>
+                <p style="text-align: center;"><p>PEDIDO: <?php echo $dado['nome'] ?></p>
+                <div id="inf"><p>VALOR R$ <?php echo $dado['preco'] ?></P></div>
+                <div id="inf"><p id="st">STATUS: <?php echo $st; ?></p></div>
                 <div><form action="./user/processos/addCarrinho.php" method="POST">
                 </form></div>
             </div> 
