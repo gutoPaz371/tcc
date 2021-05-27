f1f710ef809a4c145e230c79dc66d0a8ceae03ea
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-27 11:09:02 -0300

    implementacao quanditade add produto

--- painel/admin/crudAdm/addEstoque.php
+++ painel/admin/crudAdm/addEstoque.php
@@ -24,6 +24,25 @@
     <link rel="stylesheet" href="processos/addEstoque.css">
     <title>Adicionar Estoque</title>
 </head>
+<style>
+    #quant{
+        width: 20px;
+        height:20px;
+        float: right;
+        position: relative;
+        cursor: pointer;
+        right: 23%;border: medium;
+        border-color: black;
+        background-color: white;
+        margin: 1px 3px 0px 3px;      
+    }#quant:hover{
+        background-color: whitesmoke;
+    }#quant:active{
+        background-color: #5ddcd4;
+        width: 18px;
+        height: 18px;
+    }
+</style>
 <body>
 
 
@@ -33,23 +52,26 @@
     
     <div class="arquivos">
         <form class="caixa" action="./processos/processaAddEstoque.php" method="POST" enctype="multipart/form-data">
-    <span<?php echo $cor ?>"><?php echo $erro;?></span><br>
-        <input type="text" name="nome" placeholder="NOME"><br>
-        <input type="moeda" name="preco" placeholder="VALOR"><br>
-        <input class="arq" type="file" name="arquivo"><br>   
+            <span<?php echo $cor ?>"><?php echo $erro;?></span><br>
+            <label for="">NOME: </label>
+            <input type="text" name="nome" ><br>
+            <label for="">VALOR: </label>
+            <input type="moeda" name="preco" ><br>
+            <label for="">QUANTIDADE: </label>
+            <input id="qt" value="0" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quant" style="width: 40px;" ><div id="quant" onclick="add(this)">+</div><div id="quant" onclick="rem(this)">-</div>
+            <input class="arq" type="file" name="arquivo"><br>   
     </div>
-       
-    <div class="btn-prod">
-        <div id='circulo' class="cor" onclick="select(id)"><h1>CIRCULO</h1></div>
-        <div id='bolha' class="cor" onclick="select(id)"><h1>BOLHA</h1></div>
-        <div id='coracao' class="cor" onclick="select(id)"><h1>CORAÇÃO</h1></div>
-        <div id='estrela' class="cor" onclick="select(id)"><h1>ESTRELA</h1></div>          
-    </div>
-              
-    <div class="btn-adicionar">
-        <button id="tipo" name="tipo" value="" type="submit">ADICIONAR</button>
-        </form><a href="../logado.php"><button class="tipo">VOLTAR</button></a>
-    </div>
+        
+        <div class="btn-prod">
+            <div id='circulo' class="cor" onclick="select(id)"><h1>CIRCULO</h1></div>
+            <div id='bolha' class="cor" onclick="select(id)"><h1>BOLHA</h1></div>
+            <div id='coracao' class="cor" onclick="select(id)"><h1>CORAÇÃO</h1></div>
+            <div id='estrela' class="cor" onclick="select(id)"><h1>ESTRELA</h1></div>          
+        </div>        
+        <div class="btn-adicionar">
+            <button id="tipo" name="tipo" value="" type="submit">ADICIONAR</button>
+            </form><a href="../logado.php"><button class="tipo">VOLTAR</button></a>
+        </div>
 </section>
         
     
@@ -59,7 +81,7 @@
     oldCor="";
     oldId="";
     selectCor="";
-    cor= "#5ddcd4";
+    cor= "red";
     function select(id){
         if(oldId.length>0 && oldId!=id){
             document.getElementById(oldId).style.borderColor=oldCor;
@@ -84,6 +106,17 @@
             }
         }
         document.getElementById('tipo').value=selectCor;
+    }
+    function add(){
+        let quant=parseInt(document.getElementById('qt').value);
+        quant=quant+1;
+        document.getElementById('qt').value=quant;   
+    }function rem(){
+        let quant=parseInt(document.getElementById('qt').value);
+        quant=quant-1;
+        if(quant>=0){
+            document.getElementById('qt').value=quant;
+        }        
     }
 </script>
 </html>
