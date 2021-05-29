3aa1ae709324655da31d2ee7c57b2634bf5c0c1c
Augusto Oliveira Paz <agustooliveirapaz350@gmail.com>
2021-05-28 22:29:31 -0300

    ATT QUANT

--- painel/admin/crudAdm/addEstoque.php
+++ painel/admin/crudAdm/addEstoque.php
@@ -58,7 +58,7 @@
             <label for="">VALOR: </label>
             <input type="moeda" name="preco" ><br>
             <label for="">QUANTIDADE: </label>
-            <input id="qt" value="0" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quant" style="width: 40px;" ><div id="quant" onclick="add(this)">+</div><div id="quant" onclick="rem(this)">-</div>
+            <input id="qt" value="1" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quant" style="width: 40px;" ><div id="quant" onclick="add(this)">+</div><div id="quant" onclick="rem(this)">-</div>
             <input class="arq" type="file" name="arquivo"><br>   
     </div>
         
@@ -114,7 +114,7 @@
     }function rem(){
         let quant=parseInt(document.getElementById('qt').value);
         quant=quant-1;
-        if(quant>=0){
+        if(quant>=1){
             document.getElementById('qt').value=quant;
         }        
     }
