4ff1c7a1f01846141caf78e1ad6c0da83ed25c86
Fran18 <redminote8fran@gmail.com>
2021-05-30 13:30:34 -0300

    A

--- produtos.php
+++ produtos.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-      <meta charset="UTF-8">
+      <meta charSset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Mania de Festa</title>
       <link rel="stylesheet" href="css/produtos.css">
