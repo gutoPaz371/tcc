6becad812940bc27e43bd1716da59cf3c6170535
Gabriel-Louzada-Marques <gabriel.m.louzada@gmail.com>
2021-06-05 19:03:52 -0300

    Correção do rodapé do sobre nós

--- Empresa.php
+++ Empresa.php
@@ -5,57 +5,159 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../tcc/css/site.css">
+    <title>Sobre nós</title>
+    <link rel="stylesheet" href="css/rodape.css">
+
+    <!-- CDN Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 
 <body>
-<!-- inicio menu -->
-<nav id="menu-1">
+<link rel="stylesheet" href="css/index.css">
+    <!-- inicio menu -->
+    <nav id="menu-1">
         <ul>
             <li><a href="../tcc/index.php">Home</a></li>
             <li><a href="####">Produtos</a></li>
             <li><a href="####">Empresa</a></li>
         </ul>
-</nav>
-<!--  final menu -->
+    </nav>
+    <!--  final menu -->
 
-    <h1>[HISTÓRIA]
-Mania de festa é uma empresa nova porém com bastante peso no histórico em relação ao trabalho com confecções de festas, e variedades para enfeites e ornamentações.
- Tudo começou com uma ideia e uma oportunidade de criar algo que pudesse tornar as celebrações da vida memoráveis. 
-No começo não tínhamos muito, a não ser uma mesinha e um notebook para realizar compras e gerenciar todas as demandas (tempos difíceis).
- Logo a paixão por decorações foi transformando aquela pequena ideia em uma escada, onde buscamos nos aperfeiçoar,
-estudar e mergulhar no mundo dos adornamentos, trazendo aos clientes produtos de qualidade.
-Aos poucos toda a história da Mania de Festa foi se tornando uma grande realização,
-somos gratos por todo apoio que recebemos até agora e queremos que você faça parte dessa historia. 
-
-[MISSÃO]
-Nosso foco é sempre trazer alegria para as festas, por isso sempre buscamos as melhores maneiras de agradar nossos clientes,
-fazendo com que a experiência vivida por eles se torne parte de nossa carreira, e isso é muito importante para nós,
-tendo sempre o intuito de levar aos clientes produtos de alta qualidade, variedades em opções e o conforto que somente na Mania de Festas ele pode encontrar.
-
-[VISÃO]
-Um sinônimo de bons tempos, e tornar-se referencia em balões personalizados para o pais inteiro na próxima década e assim tornar o ramo de decoração
-mais acessível para muitas pessoas. Planejamos chegar lá o mais rápido possível, com dedicação, estratégia e visão, mas sem nunca perder 
-o nosso icônico capricho. 
-
-
-[REDES SOCIAIS]
-Siga nossa redes sociais para acompanhar novidades exclusivas! 
+    <h1>Somos uma empresa especialista em artigos de festas, fundada em 2018. Levando alegria para você e sua festa com todo nossa dedicação, amor e carinho. Aproveite para conhecer mais sobre nós: <br>
     </h1>
 
-<footer>
-    <div class="rodape">
-        <h1>www.maniadefesta.com.br</h1>
-    </div>
-</footer>
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+    <h2>
+    <p>[HISTÓRIA]
+        Mania de festa é uma empresa nova porém com bastante peso no histórico em relação ao trabalho com confecções de festas, e variedades para enfeites e ornamentações.
+        Tudo começou com uma ideia e uma oportunidade de criar algo que pudesse tornar as celebrações da vida memoráveis.
+        No começo não tínhamos muito, a não ser uma mesinha e um notebook para realizar compras e gerenciar todas as demandas (tempos difíceis).
+        Logo a paixão por decorações foi transformando aquela pequena ideia em uma escada, onde buscamos nos aperfeiçoar,
+        estudar e mergulhar no mundo dos adornamentos, trazendo aos clientes produtos de qualidade.
+        Aos poucos toda a história da Mania de Festa foi se tornando uma grande realização,
+        somos gratos por todo apoio que recebemos até agora e queremos que você faça parte dessa historia.</p>
 
+    <p>[MISSÃO]
+        Nosso foco é sempre trazer alegria para as festas, por isso sempre buscamos as melhores maneiras de agradar nossos clientes,
+        fazendo com que a experiência vivida por eles se torne parte de nossa carreira, e isso é muito importante para nós,
+        tendo sempre o intuito de levar aos clientes produtos de alta qualidade, variedades em opções e o conforto que somente na Mania de Festas ele pode encontrar.</p>
 
+    <p>[VISÃO]
+        Um sinônimo de bons tempos, e tornar-se referencia em balões personalizados para o pais inteiro na próxima década e assim tornar o ramo de decoração
+        mais acessível para muitas pessoas. Planejamos chegar lá o mais rápido possível, com dedicação, estratégia e visão, mas sem nunca perder
+        o nosso icônico capricho.</p>
 
-    <!--  final rodapé -->
+    <p>[REDES SOCIAIS]
+    Já proporcionamos momentos felizes de várias pessoas e famílias, venha ser mais uma!
+        Siga nossa redes sociais para acompanhar novidades exclusivas!</p></h2>
+    
+    <!-- Início Footer -->
+    <footer class="page-footer font-small mdb-color lighten-3 pt-4" style="background-color: #90b2c6;">
+        <!-- Footer Links -->
+        <div class="container text-center text-md-left">
+            <!-- Grid row -->
+            <div class="row">
+                <!-- Grid column -->
+                <hr class="clearfix w-100 d-md-none">
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+                    <!-- Links -->
+                    <h5 class="font-weight-bold text-uppercase mb-4">Páginas</h5>
 
+                    <ul class="list-unstyled">
+                        <li>
+                            <p>
+                                <a class="link-footer" href="index.php">Home</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <a class="link-footer" href="user/login.php">Carrinho</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <a class="link-footer" href="painel/index.php" target="blank">Painel</a>
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <a class="link-footer" href="produtos.php">Produtos</a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Grid column -->
+                <hr class="clearfix w-100 d-md-none">
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+                    <!-- Contact details -->
+                    <h5 class="font-weight-bold text-uppercase mb-4">Onde nos encontrar?</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <p>
+                                <i class="fas fa-home mr-3"></i> R. Antônio Pontes, 1211 - Jardim Mirai, Santa Isabel do Pará - PA, 68790-000 &nbsp;&nbsp;&nbsp;Apeú, PA
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <i class="fas fa-envelope mr-3"></i> gabriel.m.louzada@gmail.com
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <i class="fas fa-phone mr-3"></i> (91) 98438-2681
+                            </p>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+                <hr class="clearfix w-100 d-md-none">
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+                    <!-- Social buttons -->
+                    <h5 class="font-weight-bold text-uppercase mb-4">Saiba Mais</h5>
+                    <div class="div-icon-1">
+                        <!-- Facebook -->
+                        <a href="https://www.facebook.com/Mania-De-Festa-Presentes-2463918703841785" type="button" target="blank" class="img-redes-sociais">
+                            <img src="img/facebook.png" alt="" width="35px">
+                        </a>
+                        <br>
+                        <!-- Instagram -->
+                        <a href="https://www.instagram.com/maniadefestasip/" type="button" target="blank" class="img-redes-sociais">
+                            <img src="img/instagram.png" alt="" width="35px">
+                        </a>
+                        <br>
+                        <!-- Whatsapp -->
+                        <a href="https://api.whatsapp.com/send?phone=5591984382681" target="blank" type="button" class="img-redes-sociais">
+                            <img src="img/whatsapp.png" alt="" width="35px">
+                        </a>
+                        <br>
+                        <!-- Mapas -->
+                        <a href="https://www.google.com/maps/place/R.+Ant%C3%B4nio+Pontes,+1211+-+Jardim+Mirai,+Santa+Isabel+do+Par%C3%A1+-+PA,+68790-000/@-1.3005153,-48.1630375,17z/data=!3m1!4b1!4m5!3m4!1s0x92a45157e89124c7:0x8f9c45c7486945a5!8m2!3d-1.3005153!4d-48.1608488?hl=pt-BR" target="blank" type="button" class="img-redes-sociais">
+                        <img src="img/google-maps.png" alt="" width="35px">
+                    </a>
+                    </div>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        <!-- Footer Links -->
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">Copyright © 2021 <a href="#"> Mania de Festa</a> - Todos os direitos reservados
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Fim Footer -->
+
+    <script src="javascript/jquery-3.6.0.min.js"></script>
+    <!-- CDN Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
