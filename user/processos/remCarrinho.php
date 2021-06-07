abf92974c1b0b5739c3317bb2dc5013d4eff13ad
gutoPaz <augustooliveirapaz350@gmail.com>
2021-06-07 12:40:09 -0300

    Att remCarrinho

--- user/processos/remCarrinho.php
+++ user/processos/remCarrinho.php
@@ -1,13 +1,15 @@
 <!--CODIGO PRODUZIDO POR AUGUSTO OLIVEIRA PAZ 201902535855-->
 
 <?php
-    include '../../Config/conexao.php';
+include '../../Config/conexao.php';
     session_start();
     $id=$_POST['id'];
     $idUser=$_SESSION['id'];
-    $sql="SELECT quantidade as quant from pedido where id=$id";
+    $sql="SELECT quantidade as quant, prod from pedido where id=$id";
     $res=mysqli_fetch_assoc($cn->query($sql));
-    if($res['quant']>1){
+    if($res['prod']==1){
+        header('location: ../carrinho.php');
+    }else if($res['quant']>1){
         $quant=$res['quant']-1;
         $cn->query("UPDATE pedido SET quantidade=$quant WHERE id=$id");
         header('location: ../carrinho.php');
@@ -15,7 +17,6 @@
         $sql2="DELETE FROM pedido WHERE id=$id AND idCliente=$idUser";
         $cn->query($sql2);
         header('location: ../carrinho.php');
-    }
-    
+    }  
 ?>
 <!--CODIGO PRODUZIDO POR AUGUSTO OLIVEIRA PAZ 201902535855-->
