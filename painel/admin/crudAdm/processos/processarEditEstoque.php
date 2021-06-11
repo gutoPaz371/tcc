1141ea0a91fe2ce0ede54e33f53ef0524a7a1dee
Italo <italonathan1234@outlook.com>
2021-06-11 20:25:19 -0300

    add CSS em processarEditEstoque

--- painel/admin/crudAdm/processos/processarEditEstoque.php
+++ painel/admin/crudAdm/processos/processarEditEstoque.php
@@ -11,58 +11,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../processos/addEstoque.css">
-    <title>Editar</title>
+    <link rel="stylesheet" href="../processos/processarEditEstoque.css">
+    <title>Editar Produto</title>
 </head>
-<style>
-    .cor{
-        width: 100px;
-        height: 30px;
-        border: 2px solid;
-        float: left;
-        margin: 2px;
-        border-radius: 20%;
-        border-color: black;
-        text-align: center; 
-        cursor: pointer;
-    }#quant{
-        width: 20px;
-        height:20px;
-        float: right;
-        position: relative;
-        cursor: pointer;
-        right: 23%;border: medium;
-        border-color: black;
-        background-color: white;
-        margin: 1px 3px 0px 3px;      
-    }#quant:hover{
-        background-color: whitesmoke;
-    }#quant:active{
-        background-color: #5ddcd4;
-        width: 18px;
-        height: 18px;
-    }
-</style>
+
 <body>
-    <form action="./updateEdit.php" enctype="multipart/form-data" method="POST" style="text-align: center;">
-        <H1>EDITAR PRODUTO</H1>
-        <label for="">Imagem</label><br>
-        <input type="file" name="arquivo"><br>
-        <label for="">Nome</label><br>
-        <input type="text" name="nome" value="<?php echo $res['nome'] ?>"><br>
-        <label for="">Preco</label><br>
-        <input type="text" name="preco" value="<?php echo $res['preco'] ?>"><br>
-        <label for="">QUANTIDADE: </label>
-        <input id="qt" value="<?php echo $res['quant']; ?>" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quant" style="width: 40px;" ><div id="quant" onclick="add(this)">+</div><div id="quant" onclick="rem(this)">-</div>
-        <br> <label for="">DESCRICAO</label><br><textarea name="descricao"><?php echo $res['descricao']; ?></textarea><c> 100/100</c>
-        <div id='circulo' class="cor" onclick="select(id)">CIRCULO</div>
-        <div id='bolha' class="cor" onclick="select(id)">BOLHA</div>
-        <div id='coracao' class="cor" onclick="select(id)">CORAÇÃO</div>
-        <div id='estrela' class="cor" onclick="select(id)">ESTRELA</div>                
+
+<section class="box">
+    <form class="caixa" action="./updateEdit.php" enctype="multipart/form-data" method="POST" style="text-align: center;">
+        <div class="nome"><H1>Editar Produto</H1></div>
+        <div class="arquivos">
+            <label for="">Imagem</label><br>
+            <input type="file" name="arquivo"><br>
+            <label for="">Nome</label><br>
+            <input type="text" name="nome" value="<?php echo $res['nome'] ?>"><br>
+            <label for="">Preço</label><br>
+            <input type="text" name="preco" value="<?php echo $res['preco'] ?>"><br>
+            <label for="">QUANTIDADE: </label>
+            <input id="qt" value="<?php echo $res['quant']; ?>" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="quant" style="width: 40px;" ><div id="quant" onclick="add(this)">+</div><div id="quant" onclick="rem(this)">-</div>
+            <br> <label for="">Descrição</label><br><textarea name="descricao"><?php echo $res['descricao']; ?></textarea><c> 100/100</c>
+        </div>
         
-        <button name="id" value="<?php echo $res['id'] ?>" >Editar</button>
+       <div class="btn-prod">
+            <div id='circulo' class="estilo" onclick="select(id)">CIRCULO</div>
+            <div id='bolha' class="estilo" onclick="select(id)">BOLHA</div>
+            <div id='coracao' class="estilo" onclick="select(id)">CORAÇÃO</div>
+            <div id='estrela' class="estilo" onclick="select(id)">ESTRELA</div>                
+        </div>
+        
+        <div class="btn-editar">
+            <button name="id" value="<?php echo $res['id'] ?>" >Salvar</button>
+        </div>      
     </form>
-    <a href="../editEstoque.php"><button>Voltar</button></a>
+</section>
+    <a class="btn-voltar" href="../editEstoque.php">Voltar</a>
 </body>
 <script>
     x=false;
